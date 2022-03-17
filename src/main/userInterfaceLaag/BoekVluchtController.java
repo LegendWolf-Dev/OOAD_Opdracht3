13819a4c1bd790a4833c39610205baa3882ce30d
LegendWolf-Dev <jochem.nieuwenhuizen@student.hu.nl>
2022-03-17 09:40:27 +0100

    Originele Opdracht3 (Werkende LuchthavenRegisteren)

---
+++ src/main/userInterfaceLaag/BoekVluchtController.java
@@ -0,0 +1,38 @@
+package main.userInterfaceLaag;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class BoekVluchtController {
+    @FXML
+    private ChoiceBox vertrekPuntBox;
+    @FXML
+    private ChoiceBox bestemmingBox;
+    @FXML
+    private ComboBox vertrekTijdBox;
+    @FXML
+    private Button confirmBoekingButton;
+    @FXML
+    private Button cancelBoekingButton;
+    @FXML
+    private Label boekingInformatie;
+    @FXML
+    private TextField voornaamTextField;
+    @FXML
+    private TextField achternaamTextField;
+    @FXML
+    private TextField straatTextField;
+    @FXML
+    private TextField huisNummerTextField;
+    @FXML
+    private TextField woonPlaatsTextField;
+    @FXML
+    private ChoiceBox klasseBox;
+    @FXML
+    private Button confirmRegisterenKlant;
+    @FXML
+    private Button cancelRegisterenKlant;
+
+
+
+}
